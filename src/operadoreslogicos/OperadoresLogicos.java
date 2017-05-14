42bed8aaa3b7c5e26a9ab9755e2113a7041b8fdc
unknown <diegodiaz.2323@gmail.com>
2017-05-13 19:47:12 -0500

    ejemplo usando operadores logicos

---
+++ src/operadoreslogicos/OperadoresLogicos.java
@@ -0,0 +1,38 @@
+
+package operadoreslogicos;
+
+
+public class OperadoresLogicos {
+
+    
+    public static void main(String[] args) {
+        
+        int edad = 15;
+        boolean ife = true;
+        //String capital = "durango";
+        
+        if((edad >=18)  && (ife == false)){
+            System.out.println("entras al antro");
+        
+        }else{
+            System.out.println("no entras eres chiquito ");
+        }
+        // OPERADORES LOGICOS
+        /*
+            || or
+            && and
+        
+        true || true = true
+        false || false = false
+        false || true = true
+        true || false = true
+      <!---------------------------------------->       
+        true && true = true
+        false && false = false
+        false && true = false
+        true && false = false      
+        */
+        
+    }
+    
+}
